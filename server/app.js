dac3b7e775c75349c5685f64760d5e254eeba206
Sophie Bonneval <maldorana@gmail.com>
2024-02-12 17:58:14 +0000

    Add GraphQL & schema

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { createHandler } = require('graphql-http/lib/use/express');
+const { schema } = require('./schema/schema');
+
 const app = express();
+app.all('/graphql', createHandler({ schema }));
 
 app.listen(4000, () => {
   console.log('server running on port 4000');
